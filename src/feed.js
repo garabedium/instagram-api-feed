f9ff6de1cf4f5e67f37811ebaac599197342f9ee
Greg Garabedian <ggarabedian09@gmail.com>
2017-02-07 18:14:24 -0200

    feat: push inst response data to model

--- src/feed.js
+++ src/feed.js
@@ -1,6 +1,13 @@
 
 function ajaxCall(){
 
+    var model = {
+        data: {
+            "media":[],
+            "thumbnails":[]
+        }
+    }
+
 	var apiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=',
 		apiToken = '5129060.14ba9b1.a0fcb87320534f7fa8e2a6f9e4009483',
 		apiAjaxUrl = apiUrl + apiToken;
@@ -11,7 +18,15 @@ function ajaxCall(){
         crossDomain: true,
         dataType: 'jsonp'
     }).done(function(response){
-        console.log(response);
+        var instagramResponse = response.data;
+
+        if(instagramResponse){
+            var media = model.data.media;
+            instagramResponse.forEach(function(el) {
+                media.push(el);
+            });
+        }
+
     });
 
 } ajaxCall();
