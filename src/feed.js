12b4764bf220f8ad374a1f932b128d4ad1ff8836
Greg Garabedian <ggarabedian09@gmail.com>
2017-02-07 18:22:03 -0200

    feat:

--- src/feed.js
+++ src/feed.js
@@ -8,25 +8,36 @@ function ajaxCall(){
         }
     }
 
-	var apiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=',
-		apiToken = '5129060.14ba9b1.a0fcb87320534f7fa8e2a6f9e4009483',
-		apiAjaxUrl = apiUrl + apiToken;
+    var control = {
+        init: function(){
 
-    $.ajax({
-        url: apiAjaxUrl,
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'jsonp'
-    }).done(function(response){
-        var instagramResponse = response.data;
+        },
+        apiCall: function(){
 
-        if(instagramResponse){
-            var media = model.data.media;
-            instagramResponse.forEach(function(el) {
-                media.push(el);
+            var apiUrl = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=',
+                apiToken = '-',
+                apiAjaxUrl = apiUrl + apiToken;
+
+            $.ajax({
+                url: apiAjaxUrl,
+                type: 'GET',
+                crossDomain: true,
+                dataType: 'jsonp'
+            }).done(function(response){
+                var instagramResponse = response.data;
+
+                if(instagramResponse){
+                    var media = model.data.media;
+                    instagramResponse.forEach(function(el) {
+                        media.push(el);
+                    });
+                }
+
             });
-        }
 
-    });
+        },
+    }
+
+
 
 } ajaxCall();
